68af85b9302dbe7e5d8b7b46ed21b8bc54d02088
Tushar Gupta <t.gupta0202@gmail.com>
2025-05-12 21:14:46 +0530

    Generating trips using AI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,16 @@
-import { useLocation } from "react-router";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
+import { Link, useLocation } from "react-router";
 import { cn } from "~/lib/utils";
 
 
 interface Props {
   title: string;
   description: string;
+  ctaText?: string;
+  ctaUrl?: string;
 }
 
-const Header = ({title, description}: Props) => {
+const Header = ({title, description, ctaText, ctaUrl}: Props) => {
     const location=useLocation();
 
   return (
@@ -15,8 +18,17 @@ const Header = ({title, description}: Props) => {
         <article>
             <h1 className={cn(" text-dark-100", location.pathname === '/' ? "text-2xl md:text-4xl font-bold " : "text-xl md:text-2xl font-semibold ")}>{title}</h1>
             <p className={cn(" text-gray-100 font-normal", location.pathname === '/' ? "text-base md:text-lg" : "text-sm md:text-lg ")}>{description}</p>
-
+            {ctaText && ctaUrl && (
+          <Link to={ctaUrl}>
+            <ButtonComponent type="button" className="!h-11 !w-full md:w-[240px] button-class">
+              <img src="/assets/icons/plus.svg" alt="plus" className="size=5" />
+              <span className="p-16-semibold text-white">{ctaText}</span>
+            </ButtonComponent>
+          </Link>
+        )}
         </article>
+
+        
     </header>
   )
 }
